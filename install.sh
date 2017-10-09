4a48b7dc7cc98bb9ad1fccd4f7fdc4e6f3f0366e
Luca Campese <me@lucacampese.net>
2017-10-09 23:25:21 +0100

    Install fonts

--- install.sh
+++ install.sh
@@ -7,5 +7,14 @@ ln -s $HOME/.dotvim/vim $HOME/.vim
 # Clone Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-# Launch vim
+# Install fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+
+cd fonts/
+./install.sh Meslo
+
+cd ..
+rm -rf fonts/
+
+# Launch Vim
 vim +PluginInstall +qall
